e0a7a59d26b5a6e874372e8643aab3c4ba97dbf0
charuka deshan <itscharukadeshan@gmail.com>
2023-06-12 13:03:21 +0530

    update the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,33 +6,32 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      daisyui: {
-        themes: [
-          {
-            mytheme: {
-              primary: "#ce37b2",
-
-              secondary: "#8b6ec9",
-
-              accent: "#38f7d4",
-
-              neutral: "#1e1d26",
-
-              "base-100": "#3b4354",
-
-              info: "#8fdcef",
-
-              success: "#13aa89",
-
-              warning: "#a3590a",
-
-              error: "rgba(250, 41, 65, 0.5)",
-            },
-          },
-        ],
-      },
-    },
+    extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#f492ab",
+
+          secondary: "#e8887f",
+
+          accent: "#eaa6c8",
+
+          neutral: "#242a3d",
+
+          "base-100": "#383838",
+
+          info: "#5ba8f1",
+
+          success: "#18bfa8",
+
+          warning: "#fa971e",
+
+          error: "#f07056",
+        },
+      },
+    ],
+  },
 };
