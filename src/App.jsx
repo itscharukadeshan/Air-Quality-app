86fd234ad329ba6ad5c3db98ec6a4080ef39ba1b
charuka deshan <itscharukadeshan@gmail.com>
2023-06-12 11:59:23 +0530

    update the env location

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import CitySearch from "./components/CitySearch";
 import { useState } from "react";
 
 export default function App() {
+  const aqiApiToken = import.meta.env.VITE_AQI_API_TOKEN;
   const [airQualityData, setAirQualityData] = useState(null);
   const [error, setError] = useState(null);
 
   const getAirQuality = async (city) => {
     try {
       const response = await fetch(
-        `https://api.waqi.info/feed/${city}/?token=dbf08cce6f3705d9a9d8349576308958cf75cbd9`
+        `https://api.waqi.info/feed/${city}/?token=${aqiApiToken}`
       );
       const data = await response.json();
       console.log(data);
@@ -30,8 +31,8 @@ export default function App() {
 
       setAirQualityData(null);
     }
-    console.log(airQualityData);
   };
+
   return (
     <div className='p-4'>
       <h1 className='text-3xl font-bold '>Air Quality Index Checker</h1>
