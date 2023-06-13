b8056293648e526fd353a487849e6b1f20a890af
charuka deshan <itscharukadeshan@gmail.com>
2023-06-13 19:33:11 +0530

    update time logic

--- src/services/calculateDiff.js
+++ src/services/calculateDiff.js
@@ -3,15 +3,17 @@
 import moment from "moment";
 
 export default function calculateDiff(time) {
-  const currentUTCTime = moment().utcOffset(time.tz);
+  const utcOffsetString = time.tz;
+  const [h, m] = utcOffsetString.split(":").map(Number);
+  const totalMinutesInUTC = h * 60 + m;
+
+  const currentUTCTime = moment().utcOffset(totalMinutesInUTC);
   const currentTime = currentUTCTime.format("YYYY-MM-DD HH:mm:ss");
-  const givenUTCTime = moment(time.s).utc(time.tz);
+  const givenUTCTime = moment(time.s).utcOffset(totalMinutesInUTC);
 
   const timeDifference = givenUTCTime.diff(currentTime);
 
   const duration = moment.duration(timeDifference);
-  const hours = duration.hours();
-  const minutes = duration.minutes();
 
   return duration;
 }
