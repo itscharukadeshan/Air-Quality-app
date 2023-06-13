bb562ae30ead943a7abee86bd034783d65bad63a
charuka deshan <itscharukadeshan@gmail.com>
2023-06-13 11:54:33 +0530

    add search sudgections

--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -1,12 +1,17 @@
 /** @format */
 
 import React, { useState } from "react";
+import { getCityNameByInput } from "../services/searchServices";
 
 const CitySearch = ({ getAirQuality }) => {
   const [inputValue, setInputValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
 
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
+  const handleInputChange = async (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+    const suggestions = await getCityNameByInput(value);
+    setSuggestions(suggestions.slice(0, 5));
   };
 
   const handleSearch = (event) => {
@@ -16,12 +21,23 @@ const CitySearch = ({ getAirQuality }) => {
   };
   return (
     <form onSubmit={handleSearch} className='mb-4 flex flex-row gap-4 py-6'>
-      <input
-        type='text'
-        placeholder='Enter city...'
-        onChange={handleInputChange}
-        className='input input-bordered input-success w-1/2 max-w-xs'
-      />
+      <div className='flex flex-col'>
+        <input
+          type='text'
+          placeholder='Enter city...'
+          value={inputValue}
+          onChange={handleInputChange}
+          className='input input-bordered input-success w-full'
+        />
+        <ul className='font-bold bg-opacity-30 bg-green-700 rounded-b-xl shadow-xl'>
+          {suggestions.map((suggestion) => (
+            <li className='p-4 shadow-2xl' key={suggestion.matching_full_name}>
+              {suggestion.matching_full_name}
+            </li>
+          ))}
+        </ul>
+      </div>
+
       <button type='submit' className='btn btn-outline btn-md btn-success'>
         Search
       </button>
--- src/services/searchServices.js
+++ src/services/searchServices.js
@@ -1,2 +1,13 @@
 /** @format */
-export const getCityNameByInput = async (inputValue) => {};
+export const getCityNameByInput = async (value) => {
+  try {
+    const response = await fetch(
+      `https://api.teleport.org/api/cities/?search=${value}`
+    );
+    const data = await response.json();
+    return data._embedded["city:search-results"];
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
