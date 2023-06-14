03125dd33a77ddf01e13c40685c3f56565fcbfee
charuka deshan <itscharukadeshan@gmail.com>
2023-06-14 14:30:39 +0530

    fix a typo

--- src/components/AirQCard.jsx
+++ src/components/AirQCard.jsx
@@ -76,7 +76,7 @@ export default function AirQCard({ data }) {
         " rounded-xl bg-clip-padding backdrop-filter backdrop-blur-lg p-8"
       }>
       <div
-        className={` flex flex-col md:flex-row gap-2 items-center font-sans pt-4 font-extrabold text-4xl text-${textColor}`}>
+        className={` flex flex-col gap-2 items-center font-sans pt-4 font-extrabold text-4xl text-${textColor}`}>
         {`${formattedCommonCityName}`}
         <div className='font-mono text-2xl font-light'>{`(${locationName[0].name})`}</div>
       </div>
