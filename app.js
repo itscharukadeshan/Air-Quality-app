7bf6dc5a0de167a9a2811e19a44d76fe1143f647
charuka deshan <itscharukadeshan@gmail.com>
2023-09-02 14:17:13 +0530

    add basic structure for actions

---
+++ app.js
@@ -0,0 +1,24 @@
+/** @format */
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.get("/health", (req, res) => {
+  res.send("ok");
+});
+
+app.get("/version", (req, res) => {
+  res.send("1");
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static("dist"));
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
