c5b2060185adf48108bd3950ab750c4e03849c8e
charuka deshan <itscharukadeshan@gmail.com>
2023-09-03 11:08:07 +0530

    Add test for the backend

--- test/app.test.jsx
+++ test/app.test.jsx
@@ -1,1 +1,8 @@
 /** @format */
+const dummy = () => 1;
+describe("Air quality app backend tests", () => {
+  test("dummy returns one", () => {
+    const result = dummy();
+    expect(result).toBe(1);
+  });
+});
